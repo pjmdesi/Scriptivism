9bfe7bb75460e3ec4497932207e7dd840a2c40c3
PJ Mullen <contact@pjm.desi>
2020-02-13 13:49:31 -0600

    Answer card functionality & mobile testing.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,103 @@
+
+let touch = false;
+function detectTouch() {
+	$('body').on('touchstart', function() {
+		touch = true;
+	});
+	return touch;
+}
+
+function saveQ(q) {
+	console.log(q);
+}
+
+$('.content .card .select').eq(0).on(touch?'touchstart':'mouseenter', function() {
+	let q = $(this);
+
+	let ans = q.find('.answer'),
+		blnk = q.find('.blank');
+
+	q.addClass('open');
+
+	console.log('opened');
+
+	ans.on(touch?'touchstart':'mouseenter', function() {
+		ans.removeClass('selected');
+		q.addClass('selectionMade');
+		$(this).addClass('selected');
+	});
+	ans.on(touch?'touchstart':'mouseup', function() {
+		if (q.hasClass('open')) {
+			ans.removeClass('selected');
+			$(this).addClass('selected');
+			q.addClass('selectionMade');
+			q.removeClass('open');
+			console.log('selection made');
+		}
+	});
+	blnk.on(touch?'touchend':'mousedown', function() {
+		if (q.height() == 88) {
+			ans.removeClass('selected');
+			q.removeClass('selectionMade');
+			console.log('cleared');
+		}
+	});
+});
+
+$('.content .card .select').eq(0).on('mouseleave', function() {
+	$(this).removeClass('open');
+});
+
+function moveQs(callback) {
+	callback();
+}
+
+function nextQ(move=true) {
+	let cont = $('#main .content');
+
+	let curr = cont.eq(0),
+		next = cont.eq(1);
+
+	// load json and prep vars for next card
+
+	let nextCard = '<div class="card"><p class="qCount">q1</p><p>I<div class="select"><div class="answer"></div><div class="answer"></div><div class="answer"></div></div>gotten over it.</p><button class="qBnt" onclick="nextQ()">Next</button></div>';
+
+	let nextNext = next.clone().html(nextCard);
+
+	next.after(nextNext);
+
+	console.log('next');
+	if (move) {
+		moveQs(function() {
+			let cont = $('#main .content');
+
+			let curr = cont.eq(0);
+
+			cont.animate({left:'-100%'}, 500, 'easeInOutBack');
+			setTimeout(function () {
+				curr.remove();
+				cont.css('left', '0%');
+			}, 550);
+		});
+	}
+}
+
+
 let changePage = (l) => {
 
 	//checks if link is external
 	if (l.indexOf('http')) {
-		l = l.split('/')[1];
+		l = l.split('/')[1]?l.split('/')[1]:l;
 		console.log(l);
 	} else {
 		window.open(l, '_blank');
 	}
 }
+
+// things to run when DOM is ready
+$(document).ready(function() {
+	detectTouch()
+})
 
 $('a').click(function(e) {
 	let href = $(this).attr('href');
